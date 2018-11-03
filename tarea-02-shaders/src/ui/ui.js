7938201cae6ff1d0e58c77855def1fc1061d713c
ppizarror <pablo.pizarro@ing.uchile.cl>
2018-11-03 22:48:10 +0100

    Agrega inicio de ui

---
+++ tarea-02-shaders/src/ui/ui.js
@@ -0,0 +1,8 @@
+/**
+ UI
+ Crea la interfaz gráfica.
+
+ @author Pablo Pizarro R. @ppizarror.com
+ @license MIT
+ */
+"use strict";
---
+++ tarea-02-shaders/src/ui/viewer.js
@@ -0,0 +1,8 @@
+/**
+ UI VIEWER
+ Administra el visualizador con Three.js.
+
+ @author Pablo Pizarro R. @ppizarror.com
+ @license MIT
+ */
+"use strict";
